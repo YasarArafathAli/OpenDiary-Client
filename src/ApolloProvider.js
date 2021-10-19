d596b3f62c97710b94e334cd0cb34970f001143c
Yasar Arafath <yasara218@outlook.com>
2021-10-19 17:22:19 +0530

    Landing page

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'https://still-bayou-44200.herokuapp.com/'
+  uri: 'http://localhost:5000'
 });
 
 const authLink = setContext(() => {
